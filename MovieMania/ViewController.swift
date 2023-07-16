0d1cf84f6390889ac33edaa963cb5f9215251a7d
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-07-15 21:30:39 -0400

    Initialize Project

---
+++ MovieMania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMania
+//
+//  Created by Deepshika Ghale on 2023-07-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
